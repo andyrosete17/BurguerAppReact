5444670f572aac50392aeca58c5af8b1fdb8b329
andy rosete <andyrosete17@gmail.com>
2020-12-25 14:00:13 +0100

    added some lazy loading, refactor of the code

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
-import { updateObject } from "../utility";
+import { updateObject } from "../../shared/utility";
 
 const initialState = {
   token: null,
@@ -39,7 +39,6 @@ const reducer = (state = initialState, action) => {
     case actionTypes.AUTH_START:
       return authStart(state, action);
     case actionTypes.AUTH_SUCCESS:
-        console.log(action);
       return authSuccess(state, action);
 
     case actionTypes.AUTH_FAIL:
